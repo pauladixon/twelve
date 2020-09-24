a1c822b2b6ffd2d1761e5ca4ba1171e8b3ac8dae
paula dixon <paulapdixon@gmail.com>
2020-09-24 13:16:42 -0700

    cleaning up code

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -11,28 +11,28 @@ class App extends React.Component {
     let ctx
     let numRadius = 200
 
-    let canvas = document.getElementById("canvas");
+    let canvas = document.getElementById("canvas")
 
     ctx = canvas.getContext('2d')
     ctx.font = '22px "Orator Std", Arial'
 
     for(let i = 0; i < 12; i++) {
-      let angle = startAngle + (i * arc);
+      let angle = startAngle + (i * arc)
       if (i%2 === 0){
-          ctx.fillStyle = "#fffff0";
-      } else ctx.fillStyle = "#bb9990";
-      ctx.beginPath();
-      ctx.arc(250, 250, 250, angle, angle + arc, false);
-      ctx.arc(250, 250, 0, angle + arc, angle, true);
-      ctx.fill();
-      ctx.save();
-      ctx.fillStyle = "#dae0e8";
+          ctx.fillStyle = "#fffff0"
+      } else ctx.fillStyle = "#bb9990"
+      ctx.beginPath()
+      ctx.arc(250, 250, 250, angle, angle + arc, false)
+      ctx.arc(250, 250, 0, angle + arc, angle, true)
+      ctx.fill()
+      ctx.save()
+      ctx.fillStyle = "#dae0e8"
       ctx.translate(250 + Math.cos(angle + arc / 2) * numRadius,
-          250 + Math.sin(angle + arc / 2) * numRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      let number = numbers[i];
-      ctx.fillText(number, -ctx.measureText(number).width / 2, 0);
-      ctx.restore();
+          250 + Math.sin(angle + arc / 2) * numRadius)
+      ctx.rotate(angle + arc / 2 + Math.PI / 2)
+      let number = numbers[i]
+      ctx.fillText(number, -ctx.measureText(number).width / 2, 0)
+      ctx.restore()
     }
   }
 
