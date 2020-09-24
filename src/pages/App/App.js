5d312ce97836cfd5bffc8bc4c44cb9afcb0a3b03
paula dixon <paulapdixon@gmail.com>
2020-09-24 13:09:10 -0700

    reduced back to 12 houses and removed animation

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,28 +4,21 @@ import './App.css'
 class App extends React.Component {
 
   async componentDidMount() {
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 
-      9, 10, 11, 12, 13, 14, 15, 16, 
-      17, 18, 19, 20, 21, 22, 23, 24];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     let startAngle = 0;
-    let arc = Math.PI / 12;
+    let arc = Math.PI / 6;
     let ctx;
     let outerRadius = 250;
     let numRadius = 200;
     let innerRadius = 0;
-    let winningNumber;
-    let winningSegment;
-    let landingSpot;
 
     let canvas = document.getElementById("canvas");
-    let playerNumber = document.querySelector("input");
-    let message = document.getElementById("message");
 
     ctx = canvas.getContext('2d')
     ctx.font = '22px "Orator Std", Arial'
 
-    for(let i = 0; i < 24; i++) {
+    for(let i = 0; i < 12; i++) {
       let angle = startAngle + (i * arc);
       if (i%2 === 0){
           ctx.fillStyle = "#fffff0";
@@ -43,7 +36,6 @@ class App extends React.Component {
       ctx.fillText(number, -ctx.measureText(number).width / 2, 0);
       ctx.restore();
     }
-    canvas.style.animation = "wheelSpin 30s linear infinite";
   }
 
   render() {
