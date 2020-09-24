010a6ecf2619a3b5d92771534d4c0c573e479ceb
paula dixon <paulapdixon@gmail.com>
2020-09-24 12:56:29 -0700

    added numbers for houses

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,6 +4,7 @@ import './App.css'
 class App extends React.Component {
 
   async componentDidMount() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     let num = 6
     let r = 225
     let startAngle = 0
