62b8aef5cea81fca6f26155fd527b3629dbbe0d5
paula dixon <paulapdixon@gmail.com>
2020-10-23 20:08:09 -0700

    successfully brought in raphael elements in a test code

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM = require('react-dom')
 import './App.css'
 
 const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('react-raphael');
@@ -56,6 +55,23 @@ class App extends React.Component {
             <canvas id="canvas" width="500px" height="500px"></canvas>
           </div>
         </div>
+        <Paper width={300} height={300}>
+          <Set>    
+            { data.map(function(ele,pos){
+                return (<Circle key={pos} x={ele.x} y={ele.y} r={ele.r} attr={ele.attr} animate={ele.animate}/>)
+              })
+            }
+          </Set>
+          <Set>
+              <Rect x={30} y={148} width={240} height={150} attr={{"fill":"#10a54a","stroke":"#f0c620","stroke-width":5}}/>
+              <Ellipse x={150} y={198} ry={40} rx={100} attr={{"fill":"#fff","stroke":"#e11032"}} glow={{width:10,fill:true,color:"#0b8ac9",opacity:1}}/>
+              <Image src="static/images/5circle.png" x={100} y={170} width={90} height={60} />
+              <Text x={150} y={258} text="同一个世界 同一个梦想" attr={{"fill":"#fff"}}/>
+              <Text x={150} y={273} text="One World One Dream" attr={{"fill":"#fff"}}/>
+              <Path d={["M150 287L150 287"]} animate={Raphael.animation({"path": ["M80 287L220 287"]},500,"<>")} attr={{"stroke":"#fff"}}/>
+              <Line x1={150} y1={290} x2={150} y2={290} animate={Raphael.animation({ x1:80, x2:220},500,"<>")} attr={{"stroke":"#fff"}}/>
+          </Set>
+        </Paper>
       </div>
     )
   }
