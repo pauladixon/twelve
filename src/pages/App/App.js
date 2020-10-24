8c5dd7e302f5fed38d7994887d56d12d68139ad4
paula dixon <paulapdixon@gmail.com>
2020-10-23 21:31:50 -0700

    adjusted number font and coloring

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -7,7 +7,7 @@ const {Raphael,Paper,Set,Circle,Ellipse,Image,Rect,Text,Path,Line} = require('re
 class App extends React.Component {
 
   async componentDidMount() {
-    const numbers = [6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7]
+    const numbers = ['⑥', '⑤', '④', '③', '②', '①', '⑫', '⑪', '⑩', '⑨', '⑧', '⑦']
 
     let startAngle = 0
     let arc = Math.PI / 6
@@ -17,17 +17,17 @@ class App extends React.Component {
     let canvas = document.getElementById("canvas")
 
     ctx = canvas.getContext('2d')
-    ctx.font = '21px Helvetica'
+    ctx.font = '26px Helvetica'
 
     for(let i = 0; i < 12; i++) {
       let angle = startAngle + (i * arc)
-      ctx.fillStyle = "rgba(445, 255, 255, .25)"
+      ctx.fillStyle = "rgba(255, 255, 255, .35)"
       ctx.beginPath()
       ctx.arc(250, 250, 250, angle, angle + arc, false)
       ctx.arc(250, 250, 0, angle + arc, angle, true)
       ctx.fill()
       ctx.save()
-      ctx.fillStyle = "rgba(255, 255, 255)"
+      ctx.fillStyle = "#ae5a41"
       ctx.translate(250 + Math.cos(angle + arc / 2) * numRadius,
           250 + Math.sin(angle + arc / 2) * numRadius)
       ctx.rotate(angle + arc / 2 + Math.PI / 2)
